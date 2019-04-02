a6edaabfb3b821c42b07127b04d77dc2054756d8
lucvorringa <lucvorringa@luca>
2019-04-02 10:10:50 +0200

    added FPS textfield

--- src/timelapser/GUI.java
+++ src/timelapser/GUI.java
@@ -1,9 +1,13 @@
 package timelapser;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -19,14 +23,36 @@ public class GUI extends JFrame{
 		
 		JLabel LBimages = new JLabel("Images:");
 		JButton BTNimages = new JButton("Choose");
-		JPanel JPnorth = new JPanel();
+		BTNimages.addActionListener(new ChooseImagesListener());
+		
+		JLabel LBfps = new JLabel("FPS:");
+		JTextField TFfps = new JTextField("");
+		TFfps.setPreferredSize( new Dimension( 100, 24 ) );
 		
 		//BorderLayout North	
+		JPanel JPnorth = new JPanel();
 		JPnorth.add(LBimages);
 		JPnorth.add(BTNimages);
 		this.getContentPane().add(JPnorth, BorderLayout.NORTH);
 		
+		//BorderLayout Center
+		JPanel JPcenter = new JPanel();
+		JPcenter.add(LBfps);
+		JPcenter.add(TFfps);
+		this.getContentPane().add(JPcenter, BorderLayout.CENTER);
 		
+	}
+	class ChooseImagesListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+	        JFileChooser chooser = new JFileChooser();
+	        int rueckgabeWert = chooser.showOpenDialog(null);
+	        if(rueckgabeWert == JFileChooser.APPROVE_OPTION){
+	            System.out.println("Die zu öffnende Datei ist: " +
+	            chooser.getSelectedFile().getName());
+	        }
+	        
+		}
 		
 	}
 	
